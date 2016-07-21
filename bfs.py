611b362ba5dd691092a580c6f3ab45fd2949ddbd
Tushar <tushariscoolster@gmail.com>
2016-07-21 19:25:00 +0530

    Code verrification

--- bfs.py
+++ bfs.py
@@ -14,14 +14,18 @@ def bfs(graph, start):
             queue.extend(graph[vertex] - visited)      
     return visited
 
-print bfs(graph, 'A') # {'B', 'C', 'A', 'F', 'D', 'E'}
+#print bfs(graph, 'A') # {'B', 'C', 'A', 'F', 'D', 'E'}
 
 
 def bfs_paths(graph, start, goal):
     queue = [(start, [start])]
     while queue:
-        (vertex, path) = queue.pop(0)
-        for next in graph[vertex] - set(path):
+        print queue
+        vertex, path = queue.pop(0)
+        print "---"+str(graph[vertex])+'---'+str(set(path))
+        non_visited_node=graph[vertex] - set(path)
+        print "---"+str(non_visited_node)
+        for next in non_visited_node:
             if next == goal:
                 yield path + [next]
             else:
@@ -35,4 +39,8 @@ def shortest_path(graph, start, goal):
     except StopIteration:
         return None
 
-print shortest_path(graph, 'A', 'F') # ['A', 'C', 'F']
+#   print shortest_path(graph, 'A', 'F') # ['A', 'C', 'F']
+
+
+
+#http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
